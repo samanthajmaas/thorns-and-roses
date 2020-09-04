17b8ac70b1cbda102281a8ef76b69c8140d386b3
samanthajmaas <samanthajmaas@gmail.comcdclear>
2020-09-04 15:54:26 -0500

    first commit

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,24 @@
+import React from "react"
+import {Route} from "react-router-dom"
+
+export const ApplicationViews = (props) => {
+    return(
+        <>
+        <Route exact path="/nurseries">
+            <></>
+        </Route>
+
+        <Route exact path="/distributors">
+            <></>
+        </Route>
+
+        <Route exact path="/retailers">
+            <></>
+        </Route>
+
+        <Route exact path="/">
+            <></>
+        </Route> 
+        </>
+    )
+}
---
+++ src/components/ThornsAndRoses.js
@@ -0,0 +1,10 @@
+import React from "react"
+import {NavBar} from "./nav/NavBar"
+import {ApplicationViews} from "./ApplicationViews"
+
+export const ThornsAndRoses = () => (
+    <>
+    <NavBar />
+    <ApplicationViews />
+    </>
+)
